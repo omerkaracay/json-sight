57d1dd52b4b76b1cfc05fcd13ccf7606e2b9890b
omerkaracay <omerkaracay@gmail.com>
2025-01-03 16:37:21 +0200

    fix: i18n default lang

--- i18n/settings.ts
+++ i18n/settings.ts
@@ -1,3 +1,3 @@
 export const locales = ["tr", "en", "de", "fr"] as const;
 export type Locale = (typeof locales)[number];
-export const defaultLocale = "tr";
+export const defaultLocale: Locale = "en";
